224d1f2eafc113ddf1dc554c62d7ccbec1dfd4dd
hackerxiao <hackerxiao@foxmail.com>
2023-09-04 13:47:54 +0800

    fix: redis优化

--- dao/redis/user.go
+++ dao/redis/user.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"cld/settings"
+	"context"
 	"errors"
 	"time"
 
@@ -22,14 +23,26 @@ func ExitCodeTimeOut(keyMode string, email string) (err error) {
 
 func SetTimeOutAndCode(keyMode, email string, code string) (err error) {
 
-	if err := rdb.Set(ctx, keyMode+email+KeyTimeout, KeyCheck, time.Minute).Err(); err != nil {
-		return err
-	}
-	if err := rdb.Set(ctx, keyMode+email, code, time.Duration(settings.Conf.Email.Expires)*time.Second).Err(); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pipe := rdb.Pipeline()
+	expire := time.Duration(settings.Conf.Email.Expires) * time.Second
+
+	key1 := keyMode + email + KeyTimeout
+	pipe.Set(ctx, key1, KeyCheck, time.Minute)
+	pipe.Expire(ctx, key1, time.Minute)
+
+	key2 := keyMode + email
+	pipe.Set(ctx, key2, code, expire)
+	pipe.Expire(ctx, key2, expire)
+
+	_, err = pipe.Exec(ctx)
+	if err != nil {
 		return err
 	}
 
-	return
+	return nil
 }
 
 func GetCaptchaByEmail(keyMode, email string) (Ocaptcha string, err error) {
@@ -41,6 +54,7 @@ func GetCaptchaByEmail(keyMode, email string) (Ocaptcha string, err error) {
 }
 
 func DelCodeByEmail(KeyMode, email string) {
-	err := rdb.Del(ctx, KeyMode+email).Err()
-	zap.L().Error(" rdb.Del(ctx,KeyEmail+email).Err()", zap.Error(err), zap.String("email", email))
+	if err := rdb.Del(ctx, KeyMode+email).Err(); err != nil {
+		zap.L().Error(" rdb.Del(ctx,KeyEmail+email).Err()", zap.Error(err), zap.String("email", email))
+	}
 }
